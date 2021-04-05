6b5c108866057977bf327412befbd702050705c0
facesun <derser@163.com>
2021-04-06 02:49:53 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -14,16 +14,16 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'http://www.lubibox.com',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '撸币盒子 - 币圈精选项目指南',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '撸币盒子 - 币圈精选项目指南',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '撸币盒子,撸币,币圈,币圈导航,撸币指南,币圈指南,虚拟币导航,虚拟币项目,最新山寨币',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
   theme: 'Light',
@@ -31,12 +31,12 @@ const c: IConfig = {
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2019-2021 lubibox.com, All Rights Reserved</div>
   `,
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?6e667a30ffa01d3f9d46c59ad1e2ec0f',
 
   // CNZZ 统计地址
   // https://www.cnzz.com/o_index.php
@@ -81,7 +81,7 @@ const c: IConfig = {
     posterImageUrls: [
       'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
     ],
-    description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
+    description: '这里收录多达 <b>${total}</b> 个优质币圈项目网站， 助您赢在币圈'
   }
 }
 
